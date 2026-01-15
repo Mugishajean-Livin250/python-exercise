a08bbd18ebcae88c9c52dcab97f0d671292cc44c
Mugisha jean Livin <mugishalivin@gmail.com>
2026-01-15 20:48:22 +0200

    update

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,6 @@
 import numpy as np
 def analyze_scores(scores):
-    total_students = len(scores)
+    total_students = scores.size
 
     mean_score = np.round(np.mean(scores), 2)
     median_score = np.round(np.median(scores), 2)
@@ -16,5 +16,8 @@ def analyze_scores(scores):
         "median_score": median_score,
         "std_deviation": std_deviation,
         "Passed students": passed,
-        "Failed students": failed
+        "Failed students": failed_percentage
     }
+scores = np.array([45, 78, 62, 49, 90, 55, 30])
+result = analyze_scores(scores)
+print(result)
